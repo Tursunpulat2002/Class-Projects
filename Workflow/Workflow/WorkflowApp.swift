ad2fe76b3afddb5536f9be21012473933d4b8c50
tursunpulat2002 <tursunpulat2002@gmail.com>
2023-01-10 11:05:25 -0500

    Added icon image

---
+++ Workflow/Workflow/WorkflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkflowApp.swift
+//  Workflow
+//
+//  Created by Tursunpulat Orolov on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
