8621fa3c53b03aaabfc6a6d86ec3fc5d95ca0b4d
tursunpulat2002 <tursunpulat2002@gmail.com>
2023-02-04 13:30:47 -0500

    Initial Pentominoes commit

---
+++ Pentominoes/Pentominoes/PentominoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PentominoesApp.swift
+//  Pentominoes
+//
+//  Created by Tursunpulat Orolov on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct PentominoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
