ea8de3d821e88eddad1068e4f22611b7748886ad
tursunpulat2002 <tursunpulat2002@gmail.com>
2023-03-17 17:27:52 -0400

    Initial commit after putting all the provided files into the project

---
+++ Pokedex/Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Tursunpulat Orolov on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Pokedex/Pokedex/Views/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Pokedex
+//
+//  Created by Tursunpulat Orolov on 3/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
