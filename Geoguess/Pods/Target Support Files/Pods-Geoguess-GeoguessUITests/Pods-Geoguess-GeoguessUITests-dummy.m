a036b0606fdbd5be859c9008d82f17242d6035f3
tursunpulat2002 <tursunpulat2002@gmail.com>
2023-04-16 12:08:54 -0400

    First push addition.

---
+++ Geoguess/Pods/Target Support Files/Pods-Geoguess-GeoguessUITests/Pods-Geoguess-GeoguessUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Geoguess_GeoguessUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Geoguess_GeoguessUITests
+@end
---
+++ Geoguess/Pods/Target Support Files/Pods-GeoguessTests/Pods-GeoguessTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GeoguessTests : NSObject
+@end
+@implementation PodsDummy_Pods_GeoguessTests
+@end
